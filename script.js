04f22e7cac5179bb47d2b7bfaf3d55d08de0a411
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-11 21:11:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-function decimalToBinary(num){
-    let binaryStr = ""
-    while(num>0){
-        let rem = num%2;
-        binaryStr = binaryStr + rem.toString()
- 
-        num = Math.floor(num/2)
-    }
+function threeSum(arr, target) {
+// write your code here
   
-    return binaryStr.split("").reverse().join("")
 }
 
 module.exports = threeSum;
