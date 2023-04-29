61254094bb9b92641451763ce701ba3bd0e7e4dd
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-29 23:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // program to convert decimal to binary
-
-// take input
-const number = parseInt(prompt('Enter a decimal number: '));
-
-// convert t
-return (number >>> 0).toString(2);
-
-//module.exports = threeSum;
+function decimalToBinary(num){
+    let binaryStr = ""
+    while(num>0){
+        let rem = num%2;
+        binaryStr = binaryStr + rem.toString()
+ 
+        num = Math.floor(num/2)
+    }
+    return binaryStr.split("").reverse().join("")
+}
