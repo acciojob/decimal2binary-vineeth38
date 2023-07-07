e92a1bce64f5e9d22f5456c47bda5cdd41c4daed
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:14:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-// program to convert decimal to binary
-const num = parseInt(prompt('Enter a decimal number: '));
-function decimalToBinary(num){
-    let binaryStr = ""
-    while(num>0){
-        let rem = num%2;
-        binaryStr = binaryStr + rem.toString()
- 
-        num = Math.floor(num/2)
-    }
-    return binaryStr.split("").reverse().join("")
+function decimalToBinary(num) {
+  // Write your code here
 }
-module.exports = decimalToBinary;
+
+window.decimalToBinary = decimalToBinary;
